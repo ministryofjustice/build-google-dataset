5391867fea14d174d24b0596ed2f411b8564a327
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2025-04-29 20:19:15 +0100

    Check if an S3 file exists

--- src/s3Utils.ts
+++ src/s3Utils.ts
@@ -3,10 +3,12 @@ import { OUTPUT_CSV } from "./config";
 import {
   S3Client,
   PutObjectCommand,
+  HeadObjectCommand,
   type S3ClientConfig,
 } from "@aws-sdk/client-s3";
 
 const s3ClientParams: S3ClientConfig = { region: process.env.AWS_REGION };
+
 export class S3Utils {
 
   // Define the S3 client parameters
@@ -24,12 +26,13 @@ export class S3Utils {
     return s3ClientParams;
   }
 
-
+  /**
+   * Checks if the S3 bucket is accessible and writable.
+   * This function will attempt to write a test file to the S3 bucket.
+   * If it fails, it will log an error message.
+   */
   public static checkWriteAccess(): void {
-
-    const s3Client = new S3Client(this.getS3ClientParams());
-
-    // Check writing a test file to build-output.
+    const s3Client = new S3Client(s3ClientParams);
     const testFileName = "write-test.txt";
     const testFileContent = "Test access to S3 bucket";
     const uploadParams = {
@@ -48,6 +51,12 @@ export class S3Utils {
       });
   }
 
+  /**
+   * Uploads the output CSV file to S3.
+   * The file is uploaded to the specified bucket and key.
+   * If the upload is successful, a success message is logged.
+   * If it fails, an error message is logged.
+   */
   public static uploadToS3(): void {
     const s3Client = new S3Client(this.getS3ClientParams());
     const fileStream = fs.createReadStream(OUTPUT_CSV);
@@ -65,5 +74,74 @@ export class S3Utils {
       .catch(err => {
         console.error(`Error uploading to S3: ${err}`);
       });
+  }
+
+  /**
+   * Checks if a file exists in the S3 bucket.
+   * @param key The key of the file to check in the S3 bucket.
+   * @returns A promise that resolves to true if the file exists, or false if it does not.
+   */
+  public static async s3FileExists(key: string): Promise<boolean> {
+    const s3Client = new S3Client(this.getS3ClientParams());
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: key,
+    };
+
+    try {
+      await s3Client.send(new HeadObjectCommand(params));
+      return true;
+    } catch (err) {
+      if (err && typeof err === 'object' && 'name' in err && err.name === 'NotFound') {
+        return false;
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  /**
+   * Polls the S3 bucket for a file with the specified key.
+   * It checks every 6 minutes for up to 24 hours.
+   * If the file is found, it resolves the promise with true.
+   * If the file is not found after 24 hours, it logs output and resets before trying again.
+   * @param key The key of the file to check in the S3 bucket.
+   * @returns A promise that resolves to true if the file is found, or rejects with an error if an error occurs.
+   *
+   * @information a request to the S3 bucket is made every 6 minutes.
+   * @information the cost of 1000 requests is $0.0004.
+   */
+  public static pollS3File(key: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      let timeoutId: string | number | NodeJS.Timeout | null | undefined = null;
+      let attempts = 0;
+      let maxAttempts = 240; // 24 hours
+      const interval = 360000; // 6 minutes
+
+      const checkFile = async () => {
+        try {
+          const exists = await this.s3FileExists(key);
+
+          if (exists) {
+            if (timeoutId) {
+              clearTimeout(timeoutId);
+            }
+            resolve(true);
+          } else if (attempts < maxAttempts) {
+            attempts++;
+            timeoutId = setTimeout(checkFile, interval);
+          } else if (attempts === maxAttempts) {
+            console.log(`File ${key} not found after ${maxAttempts} attempts.`);
+            // resetting attempts to 0 before running the next check
+            attempts = 0;
+            timeoutId = setTimeout(checkFile, interval);
+          }
+        } catch (err) {
+          reject(err);
+        }
+      };
+
+      return checkFile();
+    });
   }
 }
