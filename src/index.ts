51472dc48b9c5408876a806e128ccd5b3c6750c9
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2025-04-30 14:41:19 +0100

    Polling S3 (#5)
    * Grammar correction
    
    * Used only in local testing
    
    * Check if an S3 file exists
    
    * Add 30-day cost information
    
    * Update description
    
    * Revert function call and remove unused constant
    
    * Concise description
    
    * Use secrets for google client creds. vs a json file.
    
    * Complete polling, with copy to S3 and cleanup.
    
    ---------
    
    Co-authored-by: EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,148 @@
-import {GoogleDriveService} from './googleDriveService';
-import {CSVUtils} from './csvUtils';
-import {S3Utils} from './s3Utils';
-import {FileResult} from "./types/FileResult";
-import {GoogleAuthService} from "./googleAuthService";
-import {MigrationMapper} from "./migrationMapper";
+import fsPromises from "node:fs/promises";
+import { GoogleDriveService } from "./googleDriveService";
+import { CSVUtils } from "./csvUtils";
+import { Notify } from "./notify";
+import { S3Utils } from "./s3Utils";
+import { FileResult } from "./types/FileResult";
+import { GoogleAuthService } from "./googleAuthService";
+import { MigrationMapper } from "./migrationMapper";
+import { EMAIL_INPUT_CSV, MIGRATION_LOG_INPUT_CSV, OUTPUT_CSV } from "./config";
+
+async function buildDataset(): Promise<void> {
+  const authService = new GoogleAuthService();
+  const emails = CSVUtils.readEmailAddresses();
+  const migrationLog = CSVUtils.readMigrationLog();
+  const migrationLogService = new MigrationMapper(migrationLog as any);
+
+  const CHUNK_SIZE = 10_000;
+  const CONCURRENCY = 25; // Process 5 users concurrently
+  let accumulatedFiles: FileResult[] = [];
+
+  for (let i = 0; i < emails.length; i += CONCURRENCY) {
+    const batchEmails = emails.slice(i, i + CONCURRENCY);
+
+    console.time(`Batch ${i / CONCURRENCY + 1} - Fetching Drive files`);
+
+    const batchResults = await Promise.all(
+      batchEmails.map(async (email) => {
+        console.time(`Fetching files for ${email}`);
+
+        const driveService = new GoogleDriveService(
+          authService.getJwtForUser(email),
+        );
+        try {
+          const userFiles = await driveService.getDriveFiles();
+          console.timeEnd(`Fetching files for ${email}`);
+
+          return userFiles.map((file) => {
+            const migrationEntry = migrationLogService.getEntry(
+              email,
+              file.googlePath,
+            );
+            if (migrationEntry) {
+              const { DestinationLocation, DestinationType, MicrosoftPath } =
+                migrationEntry;
+              return {
+                ...file,
+                destinationLocation: DestinationLocation,
+                destinationType: DestinationType,
+                microsoftPath: MicrosoftPath,
+              };
+            }
+
+            return file;
+          });
+        } catch (err) {
+          console.error(`Error processing ${email}`, err);
+          return [];
+        }
+      }),
+    );
+
+    console.timeEnd(`Batch ${i / CONCURRENCY + 1} - Fetching Drive files`);
+
+    console.time(`Batch ${i / CONCURRENCY + 1} - Data Processing`);
+    // Flatten and add the entire batch at once
+    accumulatedFiles.push(...batchResults.flat());
+
+    // Write in large chunks instead of file-by-file
+    if (accumulatedFiles.length >= CHUNK_SIZE) {
+      CSVUtils.writeOutputCsv(accumulatedFiles, { append: true });
+      accumulatedFiles.length = 0; // Clear the array
+    }
+
+    console.timeEnd(`Batch ${i / CONCURRENCY + 1} - Data Processing`);
+  }
+
+  // Write any remaining files after processing all users
+  if (accumulatedFiles.length > 0) {
+    CSVUtils.writeOutputCsv(accumulatedFiles, { append: true });
+  }
+}
 
 async function main(): Promise<void> {
-	S3Utils.checkWriteAccess();
-	const authService = new GoogleAuthService();
-	const emails = CSVUtils.readEmailAddresses();
-	const migrationLog = CSVUtils.readMigrationLog();
-	const migrationLogService = new MigrationMapper(migrationLog as any);
+  const pollResults = await Promise.all([
+    S3Utils.pollS3File(EMAIL_INPUT_CSV),
+    S3Utils.pollS3File(MIGRATION_LOG_INPUT_CSV),
+  ]);
 
-	const CHUNK_SIZE = 10_000;
-	const CONCURRENCY = 25; // Process 5 users concurrently
-	let accumulatedFiles: FileResult[] = [];
+  // Polling failed if either of the results is false
+  if (pollResults.some((result) => result === false)) {
+    console.error("Polling failed. Exiting...");
 
-	for (let i = 0; i < emails.length; i += CONCURRENCY) {
-		const batchEmails = emails.slice(i, i + CONCURRENCY);
+    Notify.sendEmail("631fc88d-c6f9-4251-aaea-dd3b08713d2a", {
+        context: "Polling for input files",
+        message:
+        "Polling for the input files failed. Please check the S3 bucket.",
+    });
 
-		console.time(`Batch ${i / CONCURRENCY + 1} - Fetching Drive files`);
+    return;
+  }
 
-		const batchResults = await Promise.all(
-			batchEmails.map(async (email) => {
-				console.time(`Fetching files for ${email}`);
+  // Download and save files from S3
+  console.log("S3Utils", "Begin download process from S3...");
+  await S3Utils.pullResourcesFromS3();
 
-				const driveService = new GoogleDriveService(authService.getJwtForUser(email));
-				try {
-					const userFiles = await driveService.getDriveFiles();
-					console.timeEnd(`Fetching files for ${email}`);
+  // Delete the `/tmp/build-output/dataset.csv` file if it exists
+  try {
+    await fsPromises.unlink(`/tmp/${OUTPUT_CSV}`);
+    console.log("Deleted existing output CSV file.");
+  } catch (err: any) {
+    if (err.code !== "ENOENT") {
+      console.error("Error deleting existing output CSV file:", err);
+    } else {
+      console.log("No existing output CSV file to delete.");
+    }
+  }
 
-					return userFiles.map((file) => {
-						const migrationEntry = migrationLogService.getEntry(email, file.googlePath);
-						if (migrationEntry) {
-							const { DestinationLocation, DestinationType, MicrosoftPath } = migrationEntry;
-							return {
-								...file,
-								destinationLocation: DestinationLocation,
-								destinationType: DestinationType,
-								microsoftPath: MicrosoftPath,
-							};
-						}
+  // Ensure the folder `/tmp/build-output` exists
+  await fsPromises.mkdir("/tmp/build-output", { recursive: true });
+  console.log("Created /tmp/build-output directory.");
 
-						return file;
-					});
-				} catch (err) {
-					console.error(`Error processing ${email}`, err);
-					return [];
-				}
-			})
-		);
+  console.log("Running build dataset...");
+  await buildDataset();
 
-		console.timeEnd(`Batch ${i / CONCURRENCY + 1} - Fetching Drive files`);
+  console.log("S3Utils", "Begin upload process to S3...");
+  S3Utils.uploadToS3();
 
-		console.time(`Batch ${i / CONCURRENCY + 1} - Data Processing`);
-		// Flatten and add the entire batch at once
-		accumulatedFiles.push(...batchResults.flat());
+  // Remove local files
+  await fsPromises.rm("/tmp/resources", { recursive: true, force: true });
+  console.log("Removed local resources directory.");
+  await fsPromises.rm(`/tmp/build-output`, { recursive: true, force: true });
+  console.log("Removed local build-output directory.");
+  
+  // Move S3 resource files to a completed directory
+  await S3Utils.moveS3ResourceFilesToCompleted();
+  console.log("Moved S3 resource files to completed directory.");
 
-		// Write in large chunks instead of file-by-file
-		if (accumulatedFiles.length >= CHUNK_SIZE) {
-			CSVUtils.writeOutputCsv(accumulatedFiles, { append: true });
-			accumulatedFiles.length = 0; // Clear the array
-		}
+  Notify.sendEmail("11ff2de6-9d50-431e-91f7-44f06a261261");
 
-		console.timeEnd(`Batch ${i / CONCURRENCY + 1} - Data Processing`);
-	}
+  // If we've made it here, we have successfully run, start polling again.
+  main();
 
-	// Write any remaining files after processing all users
-	if (accumulatedFiles.length > 0) {
-		CSVUtils.writeOutputCsv(accumulatedFiles, { append: true });
-	}
-
-	console.log('CSVUtils', 'Begin upload process to S3...')
-	S3Utils.uploadToS3();
+  return;
 }
 
 main().catch((err) => {
-	console.error('Script error:', err);
+  console.error("Script error:", err);
 });
--- src/s3Utils.ts
+++ src/s3Utils.ts
@@ -1,14 +1,17 @@
-import * as fs from 'fs';
-import { OUTPUT_CSV } from "./config";
+import * as fs from "node:fs";
+import fsPromises from "node:fs/promises";
+import { EMAIL_INPUT_CSV, MIGRATION_LOG_INPUT_CSV, OUTPUT_CSV } from "./config";
 import {
   S3Client,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+  HeadObjectCommand,
   PutObjectCommand,
   type S3ClientConfig,
 } from "@aws-sdk/client-s3";
 
-const s3ClientParams: S3ClientConfig = { region: process.env.AWS_REGION };
 export class S3Utils {
-
   // Define the S3 client parameters
   public static getS3ClientParams(): S3ClientConfig {
     const s3ClientParams: S3ClientConfig = { region: process.env.AWS_REGION };
@@ -24,12 +27,13 @@ export class S3Utils {
     return s3ClientParams;
   }
 
-
+  /**
+   * Checks if the S3 bucket is accessible and writable.
+   * This function will attempt to write a test file to the S3 bucket.
+   * If it fails, it will log an error message.
+   */
   public static checkWriteAccess(): void {
-
     const s3Client = new S3Client(this.getS3ClientParams());
-
-    // Check writing a test file to build-output.
     const testFileName = "write-test.txt";
     const testFileContent = "Test access to S3 bucket";
     const uploadParams = {
@@ -48,22 +52,177 @@ export class S3Utils {
       });
   }
 
+  /**
+   * Uploads the output CSV file to S3.
+   * The file is uploaded to the specified bucket and key.
+   * If the upload is successful, a success message is logged.
+   * If it fails, an error message is logged.
+   */
   public static uploadToS3(): void {
     const s3Client = new S3Client(this.getS3ClientParams());
-    const fileStream = fs.createReadStream(OUTPUT_CSV);
+    const fileStream = fs.createReadStream(`/tmp/${OUTPUT_CSV}`);
     const uploadParams = {
       Bucket: process.env.AWS_S3_BUCKET,
-      Key: 'build-output/dataset.csv',
+      Key: "build-output/dataset.csv",
       Body: fileStream,
-      ContentType: 'text/csv'
+      ContentType: "text/csv",
     };
 
-    s3Client.send(new PutObjectCommand(uploadParams))
+    s3Client
+      .send(new PutObjectCommand(uploadParams))
       .then(() => {
-        console.log(`Successfully uploaded ${OUTPUT_CSV} to S3 bucket.`);
+        console.log(`Successfully uploaded /tmp/${OUTPUT_CSV} to S3 bucket.`);
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(`Error uploading to S3: ${err}`);
       });
+  }
+
+  /**
+   * Checks if a file exists in the S3 bucket.
+   * @param key The key of the file to check in the S3 bucket.
+   * @returns A promise that resolves to true if the file exists, or false if it does not.
+   */
+  public static async s3FileExists(key: string): Promise<boolean> {
+    const s3Client = new S3Client(this.getS3ClientParams());
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: key,
+    };
+
+    try {
+      await s3Client.send(new HeadObjectCommand(params));
+      return true;
+    } catch (err) {
+      if (
+        err &&
+        typeof err === "object" &&
+        "name" in err &&
+        err.name === "NotFound"
+      ) {
+        return false;
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  /**
+   * @description Polls the S3 bucket for a file with the specified key.
+   * It checks every 6 minutes.
+   * If the file is found, it resolves the promise with true.
+   * If the file is not found after 24 hours, it logs output and resets before trying again.
+   *
+   * Good to know...
+   * - A request to the S3 bucket is made every 6 minutes.
+   * - The cost of 1000 requests is $0.0004.
+   * - The representative cost over 30 days is $0.00288.
+   *
+   * @param key The key of the file to check in the S3 bucket.
+   * @returns A promise that resolves to true if the file is found, or rejects with an error if an error occurs.
+   */
+  public static pollS3File(key: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      let timeoutId: string | number | NodeJS.Timeout | null | undefined = null;
+      let attempts = 0;
+      // let maxAttempts = 240; // 24 hours
+      // const interval = 360000; // 6 minutes
+      let maxAttempts = 2; // 10 seconds
+      const interval = 5000; // 5 seconds
+
+      const checkFile = async () => {
+        try {
+          const exists = await this.s3FileExists(key);
+
+          if (exists) {
+            if (timeoutId) {
+              clearTimeout(timeoutId);
+            }
+            resolve(true);
+          } else if (attempts < maxAttempts) {
+            attempts++;
+            timeoutId = setTimeout(checkFile, interval);
+          } else if (attempts === maxAttempts) {
+            console.log(`File ${key} not found after ${maxAttempts} attempts.`);
+            // resetting attempts to 0 before running the next check
+            attempts = 0;
+            timeoutId = setTimeout(checkFile, interval);
+          }
+        } catch (err) {
+          console.error(`Error checking file ${key}:`, err);
+          resolve(false);
+        }
+      };
+
+      return checkFile();
+    });
+  }
+
+  public static async pullResourcesFromS3(): Promise<void> {
+    const resourceKeys = [EMAIL_INPUT_CSV, MIGRATION_LOG_INPUT_CSV];
+
+    const s3Client = new S3Client(this.getS3ClientParams());
+  
+    // Delete the local /tmp/resources folder.
+    await fsPromises.rm("/tmp/resources", { recursive: true, force: true });
+
+    // Create the directory.
+    await fsPromises.mkdir("/tmp/resources", { recursive: true });
+    
+    for (const key of resourceKeys) {
+      const downloadParams = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: key,
+      };
+
+      // Ensure the directory exists
+      const localPath = `/tmp/${key}`;
+
+      const command = new GetObjectCommand(downloadParams);
+
+      const res = await s3Client.send(command);
+
+      if (!res.Body) {
+        throw new Error(`No body in response for ${key}`);
+      }
+
+      const bodyString = await res.Body.transformToString();
+
+      // Us async fs to write file
+      await fsPromises.writeFile(localPath, bodyString);
+
+      console.log(`Downloaded ${key} to ${localPath}`);
+    }
+  }
+
+  public static async moveS3ResourceFilesToCompleted(): Promise<void> {
+    const s3Client = new S3Client(this.getS3ClientParams());
+    const resourceKeys = [EMAIL_INPUT_CSV, MIGRATION_LOG_INPUT_CSV];
+
+    const timestamp = new Date().toISOString().replace(/:/g, "-");
+    const completedDir = `completed/${timestamp}/`;
+
+    for (const key of resourceKeys) {
+      const destination = key.replace(/resources\//, completedDir);
+      const copyParams = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        CopySource: `${process.env.AWS_S3_BUCKET}/${key}`,
+        Key: destination,
+      };
+
+      await s3Client.send(new CopyObjectCommand(copyParams));
+
+      console.log(`Copied ${key} to ${destination}`);
+
+      // Delete the original file
+      const deleteParams = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: key,
+      };
+
+      await s3Client.send(new DeleteObjectCommand(deleteParams));
+
+      console.log(`Deleted original file ${key}`);
+    }
   }
 }
