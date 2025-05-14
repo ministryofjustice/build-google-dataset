86833f0cd88b454ee9e677909eaa0178d2924c32
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-05-14 17:18:10 +0100

    Derive emails from the migration log (#27)

--- src/migrationMapper.ts
+++ src/migrationMapper.ts
@@ -1,34 +1,38 @@
-
 interface MigrationEntry {
-	SourcePath: string;
-	FullPath: string;
-	MicrosoftPath: string;
-	DestinationLocation: string;
-	DestinationType: string;
+  SourcePath: string;
+  FullPath: string;
+  MicrosoftPath: string;
+  DestinationLocation: string;
+  DestinationType: string;
 }
 
 export class MigrationMapper {
-	private readonly map: Record<string, MigrationEntry> = {};
+  private readonly map: Record<string, MigrationEntry> = {};
 
-	constructor(
-		entries: MigrationEntry[]
-	) {
-		for (const entry of entries) {
-			const key = this.createKey(entry.SourcePath, entry.FullPath);
-			this.map[key] = {
-				...entry,
-				MicrosoftPath: entry.DestinationLocation.replace(/^(?!https:\/\/forms\.office\.com\/)https:\/\/[^\/]+/, '')
-			}
-		}
-	}
+  public readonly emails: string[] = [];
 
+  constructor(entries: MigrationEntry[]) {
+    for (const entry of entries) {
+      const key = this.createKey(entry.SourcePath, entry.FullPath);
+      this.map[key] = {
+        ...entry,
+        MicrosoftPath: entry.DestinationLocation.replace(
+          /^(?!https:\/\/forms\.office\.com\/)https:\/\/[^\/]+/,
+          "",
+        ),
+      };
+      if (!this.emails.includes(entry.SourcePath)) {
+        this.emails.push(entry.SourcePath);
+      }
+    }
+  }
 
-	private createKey(sourcePath: string, fullPath: string): string {
-		return `${sourcePath}::${fullPath}`;
-	}
+  private createKey(sourcePath: string, fullPath: string): string {
+    return `${sourcePath}::${fullPath}`;
+  }
 
-	public getEntry(sourcePath: string, fullPath: string): MigrationEntry {
-		const key = this.createKey(sourcePath, fullPath);
-		return this.map[key];
-	}
+  public getEntry(sourcePath: string, fullPath: string): MigrationEntry {
+    const key = this.createKey(sourcePath, fullPath);
+    return this.map[key];
+  }
 }
