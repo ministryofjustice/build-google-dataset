90a6e1fede8c7bce6116047db398eaa46f3b35d9
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-05-14 16:49:29 +0100

    Handle special chars.

--- src/migrationMapper.ts
+++ src/migrationMapper.ts
@@ -1,34 +1,50 @@
-
 interface MigrationEntry {
-	SourcePath: string;
-	FullPath: string;
-	MicrosoftPath: string;
-	DestinationLocation: string;
-	DestinationType: string;
+  SourcePath: string;
+  FullPath: string;
+  MicrosoftPath: string;
+  DestinationLocation: string;
+  DestinationType: string;
 }
 
 export class MigrationMapper {
-	private readonly map: Record<string, MigrationEntry> = {};
+  private readonly map: Record<string, MigrationEntry> = {};
 
-	constructor(
-		entries: MigrationEntry[]
-	) {
-		for (const entry of entries) {
-			const key = this.createKey(entry.SourcePath, entry.FullPath);
-			this.map[key] = {
-				...entry,
-				MicrosoftPath: entry.DestinationLocation.replace(/^(?!https:\/\/forms\.office\.com\/)https:\/\/[^\/]+/, '')
-			}
-		}
-	}
+  // An object of characters.
+  // Keys are the characters that the Google API returns,
+  // Values are what the migration tool returns.
+  private readonly charSubstitutes: { [key: string]: string } = {
+    "´": "?",
+  };
 
+  constructor(entries: MigrationEntry[]) {
+    for (const entry of entries) {
+      const key = this.createKey(entry.SourcePath, entry.FullPath);
+      this.map[key] = {
+        ...entry,
+        MicrosoftPath: entry.DestinationLocation.replace(
+          /^(?!https:\/\/forms\.office\.com\/)https:\/\/[^\/]+/,
+          "",
+        ),
+      };
+    }
+  }
 
-	private createKey(sourcePath: string, fullPath: string): string {
-		return `${sourcePath}::${fullPath}`;
-	}
+  private replaceAll(
+    haystack: string,
+    replaceObject: { [key: string]: string },
+  ): string {
+    return Object.keys(replaceObject).reduce(
+      (f, s, i) => `${f}`.replace(new RegExp(s, "ig"), replaceObject[s]),
+      haystack,
+    );
+  }
 
-	public getEntry(sourcePath: string, fullPath: string): MigrationEntry {
-		const key = this.createKey(sourcePath, fullPath);
-		return this.map[key];
-	}
+  private createKey(sourcePath: string, fullPath: string): string {
+    return `${sourcePath}::${this.replaceAll(fullPath, this.charSubstitutes)}`;
+  }
+
+  public getEntry(sourcePath: string, fullPath: string): MigrationEntry {
+    const key = this.createKey(sourcePath, fullPath);
+    return this.map[key];
+  }
 }
